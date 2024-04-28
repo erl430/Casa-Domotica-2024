4880604490f4ca1ed78a48771daf2f52b89272da
erl430 <60577899+erl430@users.noreply.github.com>
2024-04-28 21:09:59 +0200

    Update sensorLuz.ino

--- sensorLuz.ino
+++ sensorLuz.ino
@@ -1,20 +1,22 @@
-String sensorLuz = "A0";  // variables
+// variables
 int led = 8;
 
 void setup()
 {
-  pinMode(sensorLuz, INPUT);    // inicializamos sensor de luz como pin de entrada
+  pinMode(A1, INPUT);    // inicializamos sensor de luz como pin de entrada
   pinMode(led, OUTPUT);    // inicializamos led como pin de salida
   Serial.begin(9600);
 }
 
 void loop()
 {
-  if (analogRead(sensorLuz) < 700) {               // si el sensor tiene un valor inferior a 700 (detecta mucha luz) 
+  if (analogRead(A1) < 700) {               // si el sensor tiene un valor inferior a 700 (detecta mucha luz) 
     digitalWrite(led, LOW);                        // las luces se apagan
+    Serial.println("se apagan las luces");
   } else {
     digitalWrite(led, HIGH);                       // y si no, las luces se encienden
+    Serial.println("se encienden las luces");
   }
-  Serial.println(analogRead(A0));                   // imprime por pantalla el valor del sensor de luz
+  Serial.println(analogRead(A1));                   // imprime por pantalla el valor del sensor de luz
   delay(1000);                                      // espera de 1 segundo
 }
